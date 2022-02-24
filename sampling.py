4d4eccd10ed6650b4cf4d6ddb8d00f8cf9e87cb6
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-24 12:51:13 +0000

    use cond to set the size of prior sample
    rather than assuming a fixed size now that we have extra inputs to use

--- sampling.py
+++ sampling.py
@@ -400,12 +400,12 @@ def get_pc_sampler(sde, shape, predictor, corrector, inverse_scaler, snr,
     """
     with torch.no_grad():
       # Initial sample
-      x = sde.prior_sampling(shape).to(device)
+      x = sde.prior_sampling(cond.shape).to(device)
       timesteps = torch.linspace(sde.T, eps, sde.N, device=device)
 
       for i in range(sde.N):
         t = timesteps[i]
-        vec_t = torch.ones(shape[0], device=t.device) * t
+        vec_t = torch.ones(cond.shape[0], device=t.device) * t
         x, x_mean = corrector_update_fn(x, cond, vec_t, model=model)
         x, x_mean = predictor_update_fn(x, cond, vec_t, model=model)
 
