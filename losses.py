a773083496fc6d4f2c8cdcf438fa94dc9350b59d
Kyle Mills <kyle.mills@1qbit.com>
2022-06-06 16:02:38 +0000

    updated losses

--- losses.py
+++ losses.py
@@ -20,7 +20,7 @@ import torch
 import torch.optim as optim
 import numpy as np
 from models import utils as mutils
-from sde_lib import VESDE, VPSDE
+from sde_lib import VESDE, VPSDE, NUMSDE
 
 
 def get_optimizer(config, params):
@@ -148,6 +148,38 @@ def get_ddpm_loss_fn(vpsde, train, reduce_mean=True):
   return loss_fn
 
 
+
+def get_score_matching_loss_fn(sde, train, reduce_mean=True):
+  """Legacy code to reproduce previous results on DDPM. Not recommended for new work."""
+  assert isinstance(sde, NUMSDE), "DDPM training only works for VPSDEs."
+
+  reduce_op = torch.mean if reduce_mean else lambda *args, **kwargs: 0.5 * torch.sum(*args, **kwargs)
+
+  def loss_fn(model, batch):
+    model_fn = mutils.get_model_fn(model, train=train)
+    ts = torch.randint(0, vpsde.N, (batch.shape[0],), device=batch.device)
+    
+
+
+    
+    
+    
+    
+    sqrt_alphas_cumprod = vpsde.sqrt_alphas_cumprod.to(batch.device)
+    sqrt_1m_alphas_cumprod = vpsde.sqrt_1m_alphas_cumprod.to(batch.device)
+    noise = torch.randn_like(batch)
+    perturbed_data = sqrt_alphas_cumprod[labels, None, None, None] * batch + \
+                     sqrt_1m_alphas_cumprod[labels, None, None, None] * noise
+    score = model_fn(perturbed_data, labels)
+    losses = torch.square(score - noise)
+    losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1)
+    loss = torch.mean(losses)
+    return loss
+
+  return loss_fn
+
+
+
 def get_step_fn(sde, train, optimize_fn=None, reduce_mean=False, continuous=True, likelihood_weighting=False):
   """Create a one-step training/evaluation function.
 
@@ -171,6 +203,8 @@ def get_step_fn(sde, train, optimize_fn=None, reduce_mean=False, continuous=True
       loss_fn = get_smld_loss_fn(sde, train, reduce_mean=reduce_mean)
     elif isinstance(sde, VPSDE):
       loss_fn = get_ddpm_loss_fn(sde, train, reduce_mean=reduce_mean)
+    elif isinstance(sde, NUMSDE):
+      loss_fn = get_score_matching_loss_fn(sde, train, reduce_mean=reduce_mean)
     else:
       raise ValueError(f"Discrete training for {sde.__class__.__name__} is not recommended.")
 
