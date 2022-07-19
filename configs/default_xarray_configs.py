f9df0fc735fc6b26cccbe720d8152fbb26a16bfd
Henry Addison <henryaddison@users.noreply.github.com>
2022-07-19 15:36:44 +0100

    reduce default training batch size to match increase in image size

--- configs/default_xarray_configs.py
+++ configs/default_xarray_configs.py
@@ -6,7 +6,7 @@ def get_default_configs():
   config = ml_collections.ConfigDict()
   # training
   config.training = training = ml_collections.ConfigDict()
-  config.training.batch_size = 64#128
+  config.training.batch_size = 16#128
   training.n_iters = 1300001
   training.snapshot_freq = 50000
   training.log_freq = 50
