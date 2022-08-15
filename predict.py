44c434a0097387ca759cee69baddb209b9f6e2b8
Henry Addison <henryaddison@users.noreply.github.com>
2022-08-15 10:31:04 +0100

    remove config arg that got added during abseil experiments

--- predict.py
+++ predict.py
@@ -125,7 +125,7 @@ def load_config(config_name, sde):
 
 @app.command()
 @slack_sender(webhook_url=os.getenv("KK_SLACK_WH_URL"), channel="general")
-def main(workdir: Path, config: Path = typer.Option(...), dataset: str = typer.Option(...), dataset_split: str = "val", sde: SDEOption = SDEOption.subVPSDE, config_name: str = "xarray_cncsnpp_continuous", checkpoint_id: int = typer.Option(...), image_size: int = None, batch_size: int = None, num_samples: int = 3, map_features: int = None):
+def main(workdir: Path, dataset: str = typer.Option(...), dataset_split: str = "val", sde: SDEOption = SDEOption.subVPSDE, config_name: str = "xarray_cncsnpp_continuous", checkpoint_id: int = typer.Option(...), image_size: int = None, batch_size: int = None, num_samples: int = 3, map_features: int = None):
     config = load_config(config_name, sde)
     config.data.dataset_name = dataset
     if map_features is not None:
